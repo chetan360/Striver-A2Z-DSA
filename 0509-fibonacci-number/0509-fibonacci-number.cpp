eb9e79eed2a57769526afc6eaeecf8cf9bb338f3
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-06-26 13:19:54 +0530

    Time: 0 ms (100%), Space: 8.2 MB (16.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
+    int memo(int n, vector<int> &dp) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(dp[n]!=-1) return dp[n]; //already present no function call 
+        return dp[n] = memo(n-1, dp)+memo(n-2, dp);
+    }
+
     int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        vector<int> dp(n+1,-1);
+        return memo(n, dp);
     }
 };
