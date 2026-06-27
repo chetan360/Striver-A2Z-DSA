d0a99bde1a05d8ba8f750701cbf174cf2decd432
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-06-27 12:01:40 +0530

    Time: 0 ms (100%), Space: 9.9 MB (77.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -5,7 +5,7 @@ public:
         int i = 0, j = n-1;
         while(i<j) {
             while(i<n && !isalnum(s[i])) i++; //isalnum checks is it digit or alphabate
-            while(j>=0 && !isalnum(s[j])) j--; //we inc i, j is it is not digit or alphabate
+            while(j>=0 && !isalnum(s[j])) j--; //skiping s[i],s[j] if not alphanumeric
             if(i<j) {
                 if(tolower(s[i])!=tolower(s[j])) return false;
             }
