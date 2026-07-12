854ff8fcbced766bff63f34830a1d2d2006b0180
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-07-12 13:51:57 +0530

    Time: 75 ms (16.81%), Space: 50.8 MB (10.13%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
+        //for store number and its rank
         unordered_map<int, int> numRank;
+        //remove duplicates and sort arr 
         set<int> nums(arr.begin(), arr.end());
         
         int rank=1;
+        //key=num, value=rank
         for(auto x: nums) 
             numRank[x] = rank++;
         for(int i=0; i<arr.size(); i++) 
