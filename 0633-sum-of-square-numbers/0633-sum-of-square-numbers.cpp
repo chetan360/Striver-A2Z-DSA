5e920ed7d497996535e5dac2cc069bdceb3f2ea2
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-07-20 23:40:49 +0530

    Time: 0 ms (100%), Space: 7.8 MB (57.13%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     bool judgeSquareSum(int c) {
         for(long a=0; a*a<=c; a++) {
+            // pythagorous thm jesa
+            // h^2 = a^2+b^2
+            // sqrt(h^2-a^2) = c
             double b = sqrt(c-a*a);
+            // b sirf int ho to pythagorous thm satisfy ho gaya
             if(b == (int)b) return true;
         }
 
