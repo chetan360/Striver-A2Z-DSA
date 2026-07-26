8323fdf76e332626945ffd430ce55d16b1c30701
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-07-26 13:50:10 +0530

    Time: 0 ms (100%), Space: 31.6 MB (46.18%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -3,6 +3,9 @@ public:
     int maximumProduct(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        return max(nums[0]*nums[1]*nums[n-1], nums[n-1]*nums[n-2]*nums[n-3]) ;
+        // geast integer is beacomes least integer if we change sign
+        return max(
+            nums[0]*nums[1]*nums[n-1], // two smallest(for -ve case) one largest
+            nums[n-1]*nums[n-2]*nums[n-3]); // three largest
     }
 };
