eb99a79b11b12e570ded859e32cd086ac3643450
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-07-27 23:55:16 +0530

    Time: 2 ms (10.26%), Space: 12.8 MB (5.09%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    class Node {
+    public:
+        Node* children[26];
+        bool eow;
+        int childCnt;
+
+        Node() {
+            eow = false;
+            childCnt = 0;
+            for(int i=0; i<26; i++)
+                children[i] = nullptr;
+        }
+    };
+
+    void insert(Node* root, string word) {
+        Node* curr = root;
+        for(char ch: word) {
+            int idx = ch - 'a';
+            if(!curr->children[idx]) {
+                curr->children[idx] = new Node();
+                curr->childCnt++;
+            }
+            curr = curr->children[idx];
+        }
+        curr->eow = true;
+    }
+
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+
+        Node* root = new Node();
+        for(string word: strs)
+            insert(root, word);
+        
+        string commPrefix = "";
+        Node* curr = root;
+        string searchWord = strs[0];
+        for(char ch: searchWord) {
+            if(curr->childCnt != 1 || curr->eow)
+                break;
+            int idx = ch -'a';
+            commPrefix += ch;
+            curr = curr->children[idx];
+        }
+        
+        return commPrefix;
+    }
+};
