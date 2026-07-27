9fc048ee642f3566e8656f44545ce0972f84f2fb
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-07-28 00:11:25 +0530

    Time: 0 ms (100%), Space: 12 MB (26.01%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,48 +1,58 @@
 class Solution {
 public:
-    class Node {
-    public:
-        Node* children[26];
-        bool eow;
-        int childCnt;
+    // class Node {
+    // public:
+    //     Node* children[26];
+    //     bool eow;
+    //     int childCnt;
 
-        Node() {
-            eow = false;
-            childCnt = 0;
-            for(int i=0; i<26; i++)
-                children[i] = nullptr;
-        }
-    };
+    //     Node() {
+    //         eow = false;
+    //         childCnt = 0;
+    //         for(int i=0; i<26; i++)
+    //             children[i] = nullptr;
+    //     }
+    // };
 
-    void insert(Node* root, string word) {
-        Node* curr = root;
-        for(char ch: word) {
-            int idx = ch - 'a';
-            if(!curr->children[idx]) {
-                curr->children[idx] = new Node();
-                curr->childCnt++;
-            }
-            curr = curr->children[idx];
-        }
-        curr->eow = true;
-    }
+    // void insert(Node* root, string word) {
+    //     Node* curr = root;
+    //     for(char ch: word) {
+    //         int idx = ch - 'a';
+    //         if(!curr->children[idx]) {
+    //             curr->children[idx] = new Node();
+    //             curr->childCnt++;
+    //         }
+    //         curr = curr->children[idx];
+    //     }
+    //     curr->eow = true;
+    // }
 
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty()) return "";
+        // if(strs.empty()) return "";
 
-        Node* root = new Node();
-        for(string word: strs)
-            insert(root, word);
+        // Node* root = new Node();
+        // for(string word: strs)
+        //     insert(root, word);
         
+        // string commPrefix = "";
+        // Node* curr = root;
+        // string searchWord = strs[0];
+        // for(char ch: searchWord) {
+        //     if(curr->childCnt != 1 || curr->eow)
+        //         break;
+        //     int idx = ch -'a';
+        //     commPrefix += ch;
+        //     curr = curr->children[idx];
+        // }
+
         string commPrefix = "";
-        Node* curr = root;
-        string searchWord = strs[0];
-        for(char ch: searchWord) {
-            if(curr->childCnt != 1 || curr->eow)
-                break;
-            int idx = ch -'a';
-            commPrefix += ch;
-            curr = curr->children[idx];
+        sort(strs.begin(), strs.end());
+        int n=strs.size();
+        string first = strs[0], last = strs[n-1];
+        for(int i=0; i<min(first.length(), last.length()); i++) {
+            if(first[i]!=last[i])
+                return commPrefix;
+            commPrefix += first[i];
         }
         
         return commPrefix;
