eacf47a7d57e49f9c4d71dcc96b7788e418e5e0e
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-07-28 12:22:10 +0530

    Time: 11 ms (98.46%), Space: 54.2 MB (91.65%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
+        // counting sort 
         int n=s.length();
-        int partition = n/2;
-        sort(s.begin(), s.begin()+partition);
-        for(int i=0; i<partition; i++)
-            s[n-1-i] = s[i];
+        int bucket[26] = {0};
+        
+        for(int i=0; i<n/2; i++)
+            bucket[s[i]-'a']++;
+        
+        int l=0;
+        int r=n-1;
+        for(int i=0; i<26; i++) {
+            while(bucket[i]>0) {
+                char ch = i+'a';
+                s[l++] = ch;
+                s[r--] = ch;
+                bucket[i]--;
+            }
+        }
+
         return s;
     }
 };
