9d0e03d860a5452fca2fba29c4e055d3eda553d2
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-07-30 17:57:51 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int minimumPushes(string word) {
+        // int n = word.size();
+        // int ans = 0;
+        // // ith letteer position is [i/8]+1
+        // // [] is G.I.F
+        // for(int i=0; i<n; i++)
+        //     ans += (i/8)+1;
+        // return ans;
         int n = word.size();
-        int ans = 0;
-        for(int i=0; i<n; i++)
-            ans += (i/8)+1;
-        return ans;
+        int m = (n-1)/8 + 1;
+        return 4*m*(m-1) + (n-8*(m-1))*m;
     }
 };
