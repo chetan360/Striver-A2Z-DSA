4dcc749e6a9bec610d9cfcc3c7b4b2f773049319
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-07-30 18:40:09 +0530

    Time: 167 ms (10.01%), Space: 111.2 MB (19.25%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,14 +1,18 @@
 class Solution {
 private:
     void backtrack(vector<vector<int>> &res, vector<int> curr, int st, int n, int k) {
+        // if combi. is completed then  puch it back to res
         if(curr.size() == k) {
             res.push_back(curr);
             return;
         }
         for(int i=st; i<=n-(k-curr.size())+1; i++) {
+            //pusing i
             curr.push_back(i);
+            //pasing i+1 next permu. to build req. combi.
             backtrack(res, curr, i+1, n, k);
             curr.pop_back();
+            // empty curr for next combi.
         }
     }
 
@@ -16,6 +20,7 @@ public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> res;
         vector<int> curr;
+        //calculate combination of k chosen from [1,n] 
         backtrack(res, curr, 1, n, k);
         return res;
     }
