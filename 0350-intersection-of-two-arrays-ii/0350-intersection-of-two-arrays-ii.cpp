2a6e7cb7dab0a424f65c47fdef9ac271bbe1428d
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-07-31 21:02:48 +0530

    Time: 0 ms (100%), Space: 15 MB (17.57%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
+        // sort(nums1.begin(), nums1.end());
+        // sort(nums2.begin(), nums2.end());
         
-        int n1 = nums1.size(); 
-        int n2 = nums2.size();
+        // int n1 = nums1.size(); 
+        // int n2 = nums2.size();
         
-        int i=0, j=0;
+        // int i=0, j=0;
+        // vector<int> res;
+        // while(i<n1 && j<n2) {
+        //     if(nums1[i]<nums2[j]) {
+        //         i++;
+        //     } else if(nums2[j]<nums1[i]) {
+        //         j++;
+        //     } else { 
+        //         res.push_back(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        // }
+
+        // return res;
+
+        unordered_map<int,int> freq;
+        for(int x:nums1)
+            freq[x]++;
         vector<int> res;
-        while(i<n1 && j<n2) {
-            if(nums1[i]<nums2[j]) {
-                i++;
-            } else if(nums2[j]<nums1[i]) {
-                j++;
-            } else { 
-                res.push_back(nums1[i]);
-                i++;
-                j++;
+        for(int x:nums2) {
+            if(freq[x]>0) {
+                res.push_back(x);
+                freq[x]--;
             }
         }
 
