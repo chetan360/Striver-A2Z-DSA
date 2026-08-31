e54cb131cc088cecba7d21339240dee148a6878a
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-08-31 11:50:49 +0530

    Time: 4 ms (57.39%), Space: 124.6 MB (60.57%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,32 +11,32 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int>idxArr;
         int idx=1;
+        int prevIdx=0, firstIdx=0;
         ListNode* prev=head;
         ListNode* curr=head->next;
+        int minDist=INT_MAX;
         while(curr!=nullptr && curr->next!=nullptr) {
             if(prev==nullptr) continue;
             if(curr->val > prev->val && curr->val > curr->next->val ||
             curr->val < prev->val && curr->val < curr->next->val) {
-                idxArr.push_back(idx);
+                if(prevIdx==0) {
+                    prevIdx=idx;
+                    firstIdx=idx;
+                } else {
+                    minDist=min(minDist, idx-prevIdx);
+                    prevIdx=idx;
+                }
             }
             prev=curr;
             curr=curr->next;
             idx++;
         }
         
-        int n=idxArr.size();
-        if(n<2) {
-            return {-1, -1};
-        }
+        
+        if(minDist==INT_MAX) return {-1,-1}; 
+        int maxDist=prevIdx-firstIdx;
 
-        int maxima=idxArr.back()-idxArr.front();
-        int minima=INT_MAX;
-        for(int i=1; i<n; i++) {
-            minima=min(minima, idxArr[i]-idxArr[i-1]);
-        }
-
-        return {minima, maxima};
+        return {minDist, maxDist};
     }
 };
