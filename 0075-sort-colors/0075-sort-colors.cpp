048bc33600e3654e8422b68c1b535cfb23926bda
Chetan Mankar <97326774+chetan360@users.noreply.github.com>
2026-09-07 22:31:16 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.4%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,13 +4,14 @@ public:
         //dutch national flag algorithm
         int low=0, mid=0, high=nums.size()-1;
         while(mid<=high) {
-            if(nums[mid]==0) {
+            if(nums[mid]==0) { //0's in 0 to low-1
                 swap(nums[low], nums[mid]);
                 low++;
                 mid++;
-            } else if(nums[mid]==1) {
+            } else if(nums[mid]==1) { //1's in low to mid-1
                 mid++;
-            } else {
+            //mid to high (unsorted)
+            } else { //2's in high+1 to n-1
                 swap(nums[mid], nums[high]);
                 high--;
             }
